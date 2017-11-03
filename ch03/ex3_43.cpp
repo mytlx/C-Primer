9f65469264dcb49d597aaa418fb8e3417afb4829
mytlx <291132618@qq.com>
2017-11-03 22:41:33 +0800

    Update ex3_43.cpp

--- ch03/ex3_43.cpp
+++ ch03/ex3_43.cpp
@@ -6,8 +6,8 @@ int main()
     int ia[3][4] = { 0,1,2,3,4,5,6,7,8,9,10,11 };
 
     // range for
-    for (auto &row : ia)
-        for (auto col : row)
+    for (const int(&row)[4] : ia)
+        for (int col : row)
             cout << col << " ";
     cout << endl << endl;
 
